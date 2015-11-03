9a6e2ab7011d4f108d328640f49b7a510bda70d6
OaSiis <max.charbonnelle@gmail.com>
2015-11-03 12:16:09 +0100

    add User entity

---
+++ src/User.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maximilien
+ * Date: 03/11/2015
+ * Time: 12:05
+ */
+
+namespace ABC\eticket;
+
+/**
+ * Class User
+ * @package ABC\eticket
+ *
+ * @Entity
+ * @Table(name="user")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @Id
+     * @GeneratedValue(strategy="AUTO")
+     * @Column(name="id", type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @Column(name="username", type="string", length=50, unique=true)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @Column(name="password", type="string", length=50)
+     */
+    private $password;
+
+    /**
+     * @var boolean
+     *
+     * @column(name="role", type="boolean")
+     */
+    private $role;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param boolean $role
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+
+}
