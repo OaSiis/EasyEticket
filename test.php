0ccf3a68828e9744d0b4a344fa4568bdec2562e6
Adrien <adrien.bonnet@hotmail.fr>
2015-11-03 19:03:09 +0100

    pdf generator + qrcode

--- test.php
+++ test.php
@@ -1,1 +1,15 @@
-kdnfrnf
+<?php
+require __DIR__.'vendor/autoload.php';
+$mandrill = new Mandrill('S_9Qzezv5AZ5k1ie7ZTZpw');
+
+$message = new stdClass();
+$message->html = "html message";
+$message->text = "text body";
+$message->subject = "email subject";
+$message->from_email = "lenaindidi@hotmail.fr";
+$message->from_name  = "From Name";
+$message->to = array(array("email" => "axl.78@hotmail.fr"));
+$message->track_opens = true;
+
+$response = $mandrill->messages->send($message);
+?>
